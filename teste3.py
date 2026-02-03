2e0bcec7d7f4ca2d557e5d4811a069debb5a1969
Carlos Eduardo <cesr00904@gmail.com>
2026-02-03 18:09:57 -0300

    Primeiro commit: Verificador de altura

---
+++ teste3.py
@@ -0,0 +1,15 @@
+print("bem indo a montanha russa!")
+
+altura = int(input("qual é sua altura?\n"))
+
+if altura > 120:
+    print("bem indo!")  
+    idade = int(input("qual é sua idade?\n"))
+    if idade >= 18:
+        print("o valor do ingresso sera de 12")
+    elif idade >12 <18:
+        print("o valor do ingresso sera de 9")
+    else:
+        print("o valor do ingresso sera de 7")
+else:
+    print("preisa cresce mais!")
