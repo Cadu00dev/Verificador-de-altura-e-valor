be51c2f744a282d966af9185d1623e5764b205b4
Carlos Eduardo <cesr00904@gmail.com>
2026-02-03 18:13:26 -0300

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,9 +1,9 @@
-print("bem indo a montanha russa!")
+print("bem vindo a montanha russa!")
 
 altura = int(input("qual é sua altura?\n"))
 
 if altura > 120:
-    print("bem indo!")  
+    print("bem vindo!")  
     idade = int(input("qual é sua idade?\n"))
     if idade >= 18:
         print("o valor do ingresso sera de 12")
@@ -12,4 +12,4 @@ if altura > 120:
     else:
         print("o valor do ingresso sera de 7")
 else:
-    print("preisa cresce mais!")
+    print("precisa crescer mais!")
