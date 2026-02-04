638dc9e888d0799f045665bc7fd907ffef8779fe
Carlos Eduardo <cesr00904@gmail.com>
2026-02-04 16:05:48 -0300

    atualização do código

--- teste3.py
+++ teste3.py
@@ -1,19 +1,22 @@
 print("bem vindo a montanha russa!")
 
 altura = int(input("qual é sua altura?\n"))
+conta = 0
 
 if altura > 120:
     print("bem vindo!")  
     idade = int(input("qual é sua idade?\n"))
-    if idade >= 18:
-        conta = 12
-        print("o valor do ingresso sera de 12$")
-    elif idade >12 <18:
-        conta = 9
-        print("o valor do ingresso sera de 9$")
-    else:
+    if idade < 12:
         conta = 7
         print("o valor do ingresso sera de 7$")
+    elif idade >12 >18:
+        conta = 9
+        print("o valor do ingresso sera de 9$")
+    elif idade >= 45 and idade <=55:
+        print("sua conta sera gratuita!") 
+    else:
+        conta = 12
+        print("o valor do ingresso sera de 12$")
     fotos = input("Voce deseja adicionar fotos ao seu ingresso? Sim ou Nao\n")
     if fotos == "Sim":
         conta += 3
