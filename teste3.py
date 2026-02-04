83095d4024f777b631727f26ce30db4cc332d7ec
Carlos Eduardo <cesr00904@gmail.com>
2026-02-04 12:23:43 -0300

    atualizando codigo

--- teste3.py
+++ teste3.py
@@ -7,16 +7,16 @@ if altura > 120:
     idade = int(input("qual é sua idade?\n"))
     if idade >= 18:
         conta = 12
-        print("o valor do ingresso sera de 12")
+        print("o valor do ingresso sera de 12$")
     elif idade >12 <18:
         conta = 9
-        print("o valor do ingresso sera de 9")
+        print("o valor do ingresso sera de 9$")
     else:
         conta = 7
-        print("o valor do ingresso sera de 7")
+        print("o valor do ingresso sera de 7$")
     fotos = input("Voce deseja adicionar fotos ao seu ingresso? Sim ou Nao\n")
     if fotos == "Sim":
         conta += 3
-    print(f"O valor total da sua conta sera {conta}")
+    print(f"O valor total da sua conta sera {conta}$")
 else:
     print("preisa crescer mais!")
